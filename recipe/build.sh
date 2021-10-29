3678f5dd649cf1af49924d91c9b6fe771c2fe875
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-10-29 11:51:17 +0200

    Update build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+set -ex
 export CFLAGS="$CFLAGS -std=c99 -fPIC"
 
 if [ "${mpi}" == "openmpi" ]; then
     export OMPI_MCA_plm=isolated
     export OMPI_MCA_btl_vader_single_copy_mechanism=none
     export OMPI_MCA_rmaps_base_oversubscribe=yes
+fi
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+    WITH_TESTS=ON
+else
+    WITH_TESTS=OFF
 fi
 
 WORK=$PWD
@@ -19,18 +26,17 @@ for shared in OFF ON; do
 
     # build & install once for static
     cmake .. \
-        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-        -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" \
+        ${CMAKE_ARGS} \
         -DCMAKE_C_FLAGS="${CFLAGS}" \
-        -DCMAKE_C_COMPILER=mpicc \
-        -DCMAKE_CXX_COMPILER=mpic++ \
+        -DCMAKE_C_COMPILER="${CC}" \
+        -DCMAKE_CXX_COMPILER="${CXX}" \
         -DCMAKE_BUILD_TYPE=RELEASE \
         -DTPL_PARMETIS_INCLUDE_DIRS="${PREFIX}/include" \
         -DTPL_PARMETIS_LIBRARIES="${PREFIX}/lib/libparmetis${SHLIB_EXT};${PREFIX}/lib/libmetis${SHLIB_EXT}" \
         -DTPL_BLAS_LIBRARIES="${PREFIX}/lib/libblas${SHLIB_EXT}" \
         -DTPL_LAPACK_LIBRARIES="${PREFIX}/lib/liblapack${SHLIB_EXT};${PREFIX}/lib/libblas${SHLIB_EXT}" \
         -Denable_blaslib=OFF \
-        -Denable_tests=ON \
+        -Denable_tests=${WITH_TESTS} \
         -Denable_doc=OFF \
         -DBUILD_SHARED_LIBS=$shared
 
@@ -43,9 +49,9 @@ for shared in OFF ON; do
     export KMP_NUM_THREADS=${OMP_NUM_THREADS}
     export MKL_NUM_THREADS=${OMP_NUM_THREADS}
     export CTEST_REGEX="-R (pdtest_1x1|pdtest_1x2|pdtest_2x1|pddrive)"
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
-    ctest ${CTEST_REGEX}
-fi
+    if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+        ctest ${CTEST_REGEX}
+    fi
 
     make install
 done
