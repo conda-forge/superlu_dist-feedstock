71f5b4248fa2920bb3f02298f0d69297dc59bc4f
Min RK <benjaminrk@gmail.com>
2024-06-03 10:16:32 +0200

    try vader

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,9 +6,9 @@ export CFLAGS="$CFLAGS -std=c99 -fPIC"
 if [ "${mpi}" == "openmpi" ]; then
     export OPAL_PREFIX=$PREFIX
     export OMPI_MCA_plm=isolated
+    export OMPI_MCA_btl=vader,self
     export OMPI_MCA_btl_vader_single_copy_mechanism=none
     export OMPI_MCA_rmaps_base_oversubscribe=yes
-    export OMPI_MCA_btl=tcp,self
 fi
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
