85cb33db4d67c9677294ed8ad0da752b06d3d54f
Min RK <benjaminrk@gmail.com>
2024-11-11 12:40:43 +0100

    LDFLAGS order is important
    in ways I don't understand

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,11 +1,17 @@
 set -exuo pipefail
 
 pushd EXAMPLE
-mpicc $LDFLAGS $CFLAGS $(pkg-config --cflags --libs superlu_dist) pddrive.c dcreate_matrix.c -o pddrive
+mpicc pddrive.c dcreate_matrix.c -o pddrive \
+  $CFLAGS \
+  $LDFLAGS \
+  $(pkg-config --cflags --libs superlu_dist)
 mpiexec -n 2 ./pddrive g20.rua
 popd
 
 pushd FORTRAN
-mpifort $LDFLAGS $FFLAGS $(pkg-config --cflags superlu_dist) -lsuperlu_dist_fortran f_pddrive.F90 -o f_pddrive
+mpifort f_pddrive.F90 -o f_pddrive \
+  $FFLAGS \
+  $LDFLAGS \
+  $(pkg-config --cflags superlu_dist) -lsuperlu_dist_fortran
 mpiexec -n 4 ./f_pddrive ../EXAMPLE/g20.rua
 popd
